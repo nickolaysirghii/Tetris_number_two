943938f000ee9e215813e1dbcb755f88e6ad1d81
nicolay <sirghiinicolai1@gmail.com>
2023-03-07 19:17:01 +0100

    added 200 divs in levt part

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-create
+
+let namesClass = [];
+for(let i=0;i<200;i++){
+    let a = "a" + i;
+    namesClass.push(a);
+};
+for(let i=0;i<200;i++){
+    let one = document.createElement("div");
+let two = document.querySelector(".leftBd");
+one.setAttribute("class","a"+i);
+one.style.backgroundColor = "black";
+one.style.width = "9.7%";
+one.style.height = "4.8%";
+two.append(one);
+};
+
+let bam = document.querySelector(".a45");
+bam.style.backgroundColor = "green";
