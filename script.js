59e8d6c7cecc78913b28a55292bf5a3c3bb9d20c
nicolay <sirghiinicolai1@gmail.com>
2023-03-08 06:33:43 +0100

    writed the first functions

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-
-let namesClass = [];
-for(let i=0;i<200;i++){
-    let a = "a" + i;
-    namesClass.push(a);
+for(let i=0;i<50;i++){
+    let one = document.createElement("div");
+    let two = document.querySelector(".infoWindow");
+    one.setAttribute("class","a"+i)
+    one.style.width = "1%";
+one.style.height = "2%";
+one.style.backgroundColor = "blue";
+two.append(one);
 };
-for(let i=0;i<200;i++){
+for(let i=50;i<250;i++){
     let one = document.createElement("div");
 let two = document.querySelector(".leftBd");
 one.setAttribute("class","a"+i);
@@ -13,6 +16,33 @@ one.style.width = "9.7%";
 one.style.height = "4.8%";
 two.append(one);
 };
+let fun1 = function(a){
+    let one = document.querySelector(".a"+ a);
+    let one2 = document.querySelector(".a"+ (a+1));
+    let one3 = document.querySelector(".a"+ (a-1));
+    let one4 = document.querySelector(".a"+ (a-2));
+    one.style.backgroundColor = "red";
+    one2.style.backgroundColor = "red";
+    one3.style.backgroundColor = "red";
+    one4.style.backgroundColor = "red";
 
-let bam = document.querySelector(".a45");
-bam.style.backgroundColor = "green";
+};
+let fun2 = function(a){
+    let one = document.querySelector(".a"+ (a-10));
+    let one2 = document.querySelector(".a"+ (a-9));
+    let one3 = document.querySelector(".a"+ (a-11));
+    let one4 = document.querySelector(".a"+ (a-12));
+    one.style.backgroundColor = "black";
+    one2.style.backgroundColor = "black";
+    one3.style.backgroundColor = "black";
+    one4.style.backgroundColor = "black";
+};
+let f2 =function(a){
+    setInterval(function(){a += 10;
+    fun1(a);fun2(a)},1000);
+};
+f2(65)
+
+
+
+
